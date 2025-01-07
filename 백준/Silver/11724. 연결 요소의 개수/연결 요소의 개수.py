91d1333584a88f131a2c7fa0796766dcfd252963
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-01-07 22:10:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 736 ms, Memory: 66088 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,35 +1,33 @@
-# 11724  연결 요소의 개수
+# 11724  연결 요소의 개수 (DFS)
 
-from collections import deque
 import sys
+sys.setrecursionlimit(100000)
+
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
 graph = [[] for _ in range(N+1)] # 그래프 초기화
-visited = [False] * (N+1)
-count = 0
+visited = [False] * (N+1) # 방문 여부
+count = 0 # 트리 탐색
 
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a] += [b] # a에 b 연결
     graph[b] += [a] # b에 a 연결 -> 양방향
 
-def bfs(start_v, visited):
-    Q = deque([start_v])
+# 깊이 우선 탐색(재귀 함수)
+def dfs(start_v, visited):
+    visited[start_v] = True
 
-    while Q:
-        c = Q.popleft()
-
-        for nx in graph[c]:
-            if not (visited[nx]):
-                visited[nx] = True
-                Q.append(nx)
+    for nx in graph[start_v]:
+        if not (visited[nx]):
+            dfs(nx, visited)
 
     return 1
 
 for i in range(1, N+1):
     if not (visited[i]):
-        count += bfs(i, visited)
+        count += dfs(i, visited)
 
 print(count)
