416c3a963f6a109c61526b4cc0f9e8cd3d582913
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-01-07 22:11:30 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 724 ms, Memory: 67060 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,7 +1,7 @@
-# 11724  연결 요소의 개수 (DFS)
+# 11724  연결 요소의 개수 (BFS)
 
+from collections import deque
 import sys
-sys.setrecursionlimit(100000)
 
 input = sys.stdin.readline
 
@@ -16,18 +16,22 @@ for _ in range(M):
     graph[a] += [b] # a에 b 연결
     graph[b] += [a] # b에 a 연결 -> 양방향
 
-# 깊이 우선 탐색(재귀 함수)
-def dfs(start_v, visited):
-    visited[start_v] = True
+# 넓이 우선 탐색
+def bfs(start_v, visited):
+    Q = deque([start_v])
 
-    for nx in graph[start_v]:
-        if not (visited[nx]):
-            dfs(nx, visited)
+    while Q:
+        c = Q.popleft()
 
-    return 1
+        for nx in graph[c]:
+            if not (visited[nx]):
+                visited[nx] = True
+                Q.append(nx)
+
+    return 1 # 트리 탐색을 마치면 1을 반환
 
 for i in range(1, N+1):
     if not (visited[i]):
-        count += dfs(i, visited)
+        count += bfs(i, visited)
 
 print(count)
