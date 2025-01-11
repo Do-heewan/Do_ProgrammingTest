0414ab0627a82b1772a9ffe48dbad26e51b5ab9e
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-01-11 18:15:01 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+for _ in range(N):
+    word = input()
+
+    if (word == "Algorithm"):
+        print(204)
+    elif (word == "DataAnalysis"):
+        print(207)
+    elif (word == "ArtificialIntelligence"):
+        print(302)
+    elif (word == "CyberSecurity"):
+        print("B101")
+    elif (word == "Network"):
+        print(303)
+    elif (word == "Startup"):
+        print(501)
+    elif (word == "TestStrategy"):
+        print(105)
