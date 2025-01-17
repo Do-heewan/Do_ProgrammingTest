c3a285c228f575ea0366dc22f9332cb51f52f44e
Do-heewan <nhw3152@gmail.com>
2025-01-17 16:04:48 +0900

    [Gold V] Title: 두 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,28 +1,30 @@
-n = int(input())
-arr = list(map(int, input().split(' ')))
-arr.sort()
+# 2470 두 용액 (두 포인터)
 
-left = 0
-right = n-1
+N = int(input())
+li = list(map(int, input().split()))
+li.sort()
 
-answer = abs(arr[left] + arr[right])
-final = [arr[left], arr[right]]
+start, end = 0, N-1
 
+answer = abs(li[start] + li[end]) # 가장 작은 합
+result = [li[start], li[end]] # 결과 리스트
 
-while left < right:
-    left_val = arr[left]
-    right_val = arr[right]
+while (start < end):
+    s_val = li[start] # 제일 작은 값
+    e_val = li[end] # 제일 큰 값
 
-    sum = left_val + right_val
-  
-    if abs(sum) < answer:
-        answer = abs(sum)
-        final = [left_val, right_val]
-        if answer == 0:
-          break
-    if sum < 0:
-        left += 1
-    else:
-        right -= 1
+    sum = s_val + e_val
 
-print(final[0], final[1])
+    if (abs(sum) < answer): # 처음의 합과 이후 연산을 통한 합 비교
+        answer = abs(sum) # 더 작은 값이 answer에 들어감
+        result = [s_val, e_val] # 그때의 두 용액
+
+        if (answer == 0): # 0이면 최솟값이기에 break
+            break
+    
+    if (sum < 0): # 음수면 작은 값을 증가시킴
+        start += 1
+    else: # 양수면 큰 값을 감소시킴
+        end -= 1
+
+print(result[0], result[1]) # 결과 리스트 출력
