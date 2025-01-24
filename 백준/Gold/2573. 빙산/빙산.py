9b4553c394cf6ce7a52caa9c6ec7aba67d13d6a9
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-01-25 02:02:50 +0900

    [Gold IV] Title: 빙산, Time: 572 ms, Memory: 216088 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,60 +1,72 @@
-import sys
+# 2573 빙산
+
 from collections import deque
+import sys
+
+sys.setrecursionlimit(10**4)
 input = sys.stdin.readline
 
+N, M = map(int, input().split())
 
-def bfs(x, y):
-    q = deque([(x, y)])
-    visited[x][y] = 1
-    seaList = []
+# 상하좌우 이동
+dx = [0 ,0, -1, 1]
+dy = [1, -1, 0, 0]
 
-    while q:
-        x, y = q.popleft()
-        sea = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if not graph[nx][ny]:
-                    sea += 1
-                elif graph[nx][ny] and not visited[nx][ny]:
-                    q.append((nx, ny))
-                    visited[nx][ny] = 1
-        if sea > 0:
-            seaList.append((x, y, sea))
-    for x, y, sea in seaList:
-        graph[x][y] = max(0, graph[x][y] - sea)
+# 매트릭스 생성
+matrix = []
+for _ in range(N):
+    matrix.append(list(map(int, input().split())))
 
-    return 1
+time = 0 # 빙하가 분리되기까지 걸리는 시간 측정
 
+while (True):
+    ice = 0
+    visited = [[False] * M for _ in range(N)] # 방문 처리
+    sea = [[0] * M for _ in range(N)] # 주변 바다 저장 리스트
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+    # 매트릭스 완전 탐색
+    for x in range(N):
+        for y in range(M):
+            if (matrix[x][y] != 0) and not (visited[x][y]): # 빙산이고 방문하지 않은 경우
+                visited[x][y] = True # 방문 표시
 
-ice = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j]:
-            ice.append((i, j))
+                Q = deque()
+                Q.append([x, y])
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-year = 0
+                while Q:
+                    cx, cy = Q.popleft()
 
-while ice:
-    visited = [[0] * m for _ in range(n)]
-    delList = []
-    group = 0
-    for i, j in ice:
-        if graph[i][j] and not visited[i][j]:
-            group += bfs(i, j)
-        if graph[i][j] == 0:
-            delList.append((i, j))
-    if group > 1:
-        print(year)
+                    for i in range(4): # 상하좌우 탐색
+                        nx = cx + dx[i]
+                        ny = cy + dy[i]
+
+                        if (matrix[nx][ny] == 0): # 인접 노드가 0이고 방문하지 않은 경우
+                            sea[cx][cy] += 1
+
+                        elif (matrix[nx][ny] != 0) and not (visited[nx][ny]): # 0이 아닌경우 -> 빙산의 일부이기에 Q에 추가
+                            visited[nx][ny] = True
+                            Q.append([nx, ny])
+
+                # Q가 비어서 while문이 종료가 된다면 빙하가 더 이상 이어져 있지 않음
+                ice += 1
+
+    for x in range(N):
+        for y in range(M):
+            if (visited[x][y]):
+                matrix[x][y] -= sea[x][y]
+
+                if (matrix[x][y] < 0):
+                    matrix[x][y] = 0
+
+    # 탐색 이후 빙하의 개수가 2개 이상이면 멈춤
+    if (ice >= 2):
         break
-    ice = sorted(list(set(ice) - set(delList)))
-    year += 1
 
-if group < 2:
-    print(0)
+    if (ice == 0):
+        time = 0
+        break
+    
+    # 시간 증가
+    time += 1
+
+print(time)
