c76616f73508fdae06062af229097d81c96b1d98
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-02 15:42:52 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,16 @@
+# 7568 덩치
+
+N = int(input())
+
+li = []
+for _ in range(N):
+    weight, height = map(int, input().split())
+    li.append([weight, height])
+
+for ix in li:
+    rank = 1
+    for jx in li:
+        if (ix[0] < jx[0]) and (ix[1] < jx[1]):
+            rank += 1
+
+    print(rank, end = " ")
