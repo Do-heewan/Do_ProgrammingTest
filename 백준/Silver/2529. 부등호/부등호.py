8cdeadb8d56952229a0daf382932b7c9f56d1020
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-05 15:56:11 +0900

    [Silver I] Title: 부등호, Time: 120 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -3,10 +3,9 @@
 N = int(input())
 op = list(input().split())
 
-visited = [False] * 10 # 
-result = [] # 결과 저장 리스트
+visited = [0] * 10
+result = []
 
-# 부등호 판별
 def check(a, b, op):
     if (op == "<"):
         if (a > b):
@@ -18,18 +17,19 @@ def check(a, b, op):
     
     return True
 
-# DFS 알고리즘
 def dfs(count, num):
-    if (count == (N+1)): # 자릿수를 만족하면
-        result.append(num) # 리스트에 추가
+    if (count == (N+1)):
+        result.append(num)
         return
     
-    for i in range(10): # 0 ~ 9까지 탐색
-        if not (visited[i]): # 이미 나온 수는 통과 (중복 존재 x)
-            if (count == 0) or (check(num[-1], str(i), op[count-1])): # 첫번째 자리 or check()가 참일 경우
-                visited[i] = True # 방문 표시
-                dfs(count+1, num + str(i)) # 다음 숫자 재귀
-                visited[i] = False # 방문 표시 해제
+    for i in range(10):
+        if (visited[i]):
+            continue
+
+        if (count == 0) or (check(num[-1], str(i), op[count-1])):
+            visited[i] = 1
+            dfs(count+1, num + str(i))
+            visited[i] = 0
 
 dfs(0, '')
 
