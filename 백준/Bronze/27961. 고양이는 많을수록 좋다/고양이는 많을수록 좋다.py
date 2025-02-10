267d631cbd83252bce0cb352680b77a652147f5d
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-10 14:35:22 +0900

    [Bronze I] Title: 고양이는 많을수록 좋다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.py
@@ -0,0 +1,19 @@
+# 27961 고양이는 많을수록 좋다
+
+N = int(input())
+
+def cat(N):
+    magic_count = 0
+    cat_num = 1
+
+    if (cat == 0):
+        return 0
+    
+    while (N > cat_num):
+        cat_num *= 2
+        magic_count += 1
+
+    if (N <= cat_num):
+        return magic_count + 1
+
+print(cat(N) if N != 0 else 0)
