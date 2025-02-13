598e96f1752f2166bba2b91c8a231cb8606b6b03
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-13 13:41:36 +0900

    [Silver I] Title: 신입 사원, Time: 2112 ms, Memory: 212376 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -13,15 +13,15 @@ for _ in range(T):
         a, b = map(int, input().split())
         people.append((a, b))
     
-    people.sort(key = lambda x : x[0]) # 성적이 낮은 순 정렬
+    people.sort(key = lambda x : x[0]) # 서류 성적이 낮은 순 정렬
 
-    price = 1
-    target = people[0][1] # 등수
+    pass_num = 1 # 통과한 사람 수
+    target = people[0][1] # 서류 성적이 젤 낮은 사람의 면접 등수
     
     # 등수 비교
     for i in range(1, len(people)):
-        if (target > people[i][1]): # 등수가 더 크다 -> 순위가 낮다 -> 성적도 낮고 순위도 낮다
-            price += 1
-            target = people[i][1]
+        if (target > people[i][1]): # 등수가 더 크다 -> 순위가 낮다 -> 서류 성적도 낮고 면접 순위도 낮다
+            pass_num += 1 # 합격자 한명 추가
+            target = people[i][1] # 타겟 변경
 
-    print(price)
+    print(pass_num)
