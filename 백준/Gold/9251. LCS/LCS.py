910bd6015384b2ea7d39c1d6908e2c8bccc8f3cc
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-19 21:01:36 +0900

    [Gold V] Title: LCS, Time: 432 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -7,4 +7,4 @@ for i in range(1, len(s1)+1):
             lcs[i][j] = lcs[i-1][j-1] + 1
         else:
             lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
-print(max(map(max,lcs)))
+print(lcs[-1][-1])
