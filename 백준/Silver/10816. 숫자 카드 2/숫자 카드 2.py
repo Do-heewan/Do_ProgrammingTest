4d6862f8f4d5cca8745a7393f8a03eaafc3566be
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-23 20:07:40 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 480 ms, Memory: 254776 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,15 +1,14 @@
-import sys
+# 10816 숫자 카드 2
+
 from collections import Counter
+import sys
+input = sys.stdin.readline
 
-# 입력 받기
-N = int(sys.stdin.readline().strip())
-N_li = list(map(int, sys.stdin.readline().split()))
+N = int(input())
+N_list = list(map(int, input().split()))
 
-M = int(sys.stdin.readline().strip())
-M_li = list(map(int, sys.stdin.readline().split()))
+M = int(input())
+M_list = list(map(int, input().split()))
 
-# N_li에 있는 숫자의 개수를 미리 세기
-count_dict = Counter(N_li)
-
-# M_li의 각 원소에 대해 개수 출력
-print(" ".join(str(count_dict[m]) if m in count_dict else "0" for m in M_li))
+count = Counter(N_list)
+print(" ".join(str(count[i]) if i in count else "0" for i in M_list))
