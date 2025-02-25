24e1eafb2ae343a7ab3483412597121d364df3a0
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-02-25 18:21:46 +0900

    [Gold V] Title: 토마토, Time: 404 ms, Memory: 164800 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,6 +1,8 @@
 # 7576 토마토
 
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 M, N = map(int, input().split())
 
@@ -12,32 +14,38 @@ for _ in range(N):
 dx = [0, 0, -1, 1]
 dy = [1 ,-1 ,0 ,0]
 
-visited = [[False] * M for _ in range(N)]
+Q = deque() # 큐 생성
+visited = [[False] * M for _ in range(N)] # 방문 표시
 
-Q = deque()
-
-count = 0
+# 익은 토마토의 위치 찾기
 for i in range(N):
     for j in range(M):
         if (mat[i][j] == 1) and not (visited[i][j]):
             visited[i][j] = True
             Q.append((i, j))
 
+# 익은 토마토들에 대해서
 while Q:
     cx, cy = Q.popleft()
 
+    # 상, 하, 좌, 우 이동
     for i in range(4):
         nx = cx + dx[i]
         ny = cy + dy[i]
 
+        # 익지 않은 토마토라면
         if (0 <= nx < N) and (0 <= ny < M) and (mat[nx][ny] == 0) and not (visited[nx][ny]):
+            # 날짜 수를 계산하기 위해 현재 토마토의 일수 + 1
             mat[nx][ny] = mat[cx][cy] + 1
-            Q.append((nx, ny))
+            Q.append((nx, ny)) # 익은 토마토 추가
 
+# matrix 전체를 돌며
 for i in range(N):
     for j in range(M):
+        # 익지 않은  토마토를 발견한 경우, 이는 모두를 익게 할 수 없다는 뜻
         if (mat[i][j] == 0):
             print(-1)
-            exit(0)
+            exit(0) # -1 출력 후 종료
 
+# mat내부에 가장 큰 수 => 모든 토마토가 익는데 걸린 시간 => 9일째 -> 8일 걸림 / 만약 max값이 1이라면 0 출력
 print((max(max(row) for row in mat) - 1) if max(mat) != 1 else 0)
