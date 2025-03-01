51c3f58c554884989731b9d2032c7b4a3baeb4fa
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-01 23:00:07 +0900

    [Silver II] Title: N과 M (9), Time: 100 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,4 +1,9 @@
+# 15663 N과 M (9)
+
+import sys
 from itertools import permutations
+
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
@@ -7,5 +12,5 @@ li.sort()
 
 perm = set(permutations(li, M))
 
-for ix in sorted(perm):
+for ix in sorted(perm): # set을 취하는 과정에서 정렬이 풀릴 수 있기 때문에 다시 한 번 정렬
     print(*ix)
