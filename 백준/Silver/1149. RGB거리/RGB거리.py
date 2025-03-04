cbd353e70eb30980e1afdbdca94ba99ba71ca170
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-04 20:10:46 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,10 +1,14 @@
 # 1149 RGB 거리
 
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 
 # 첫 번째 RGB
 li = list(map(int, input().split()))
 
+# 두 번째 RGB부터 더해가며 값을 비교
 for _ in range(N-1):
     r, g, b = map(int, input().split())
 
@@ -12,8 +16,7 @@ for _ in range(N-1):
     new_g = min(g + li[0], g + li[2])
     new_b = min(b + li[0], b + li[1])
 
+    # 새로운 리스트로 저장
     li = [new_r, new_g, new_b]
-
-    # print(li)
 
 print(min(li))
