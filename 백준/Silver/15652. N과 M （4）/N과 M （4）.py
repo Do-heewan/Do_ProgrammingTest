84c6a1addbd0d0c6efd33465e55f2755eda40975
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-07 19:53:26 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,19 @@
+# 15652 N과 M (4)
+
+from itertools import combinations
+
+N, M = map(int, input().split())
+
+s = []
+
+def dfs(x): # 앞의 숫자와 비교해야하므로 파라미터로 넘겨줌
+    if len(s) == M: # s의 길이가 m과 같으면 답 출력
+        print(*s)
+        return
+
+    for i in range(x,N+1): # 파라미터로 받은 숫자보다 같거나 커야함
+        s.append(i) # 수를 더해주고
+        dfs(i) # 더한 수를 파라미터로 가지고 dfs
+        s.pop() # 원 상태로 돌리기 위해 pop
+
+dfs(1)
