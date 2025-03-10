d4503f651d53db9af13c3e611b8a1f4d942e02ce
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-10 20:56:07 +0900

    [Gold V] Title: 평범한 배낭, Time: 5540 ms, Memory: 283624 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,22 @@
+# 12865 평범한 배낭
+
+N, K = map(int, input().split())
+
+bag = []
+for _ in range(N):
+    W, V = map(int, input().split())
+    bag.append([W, V])
+
+knapsack = [[0] * (K+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        weight = bag[i-1][0]
+        value = bag[i-1][1]
+
+        if j < weight:
+            knapsack[i][j] = knapsack[i - 1][j] #weight보다 작으면 위의 값을 그대로 가져온다
+        else:
+            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+
+print(knapsack[N][K])
