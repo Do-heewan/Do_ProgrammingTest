a01db890e8cc929b2ade75750c84106ab67f0280
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-15 17:11:01 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,25 @@
+# 11047 동전 0
+
+N, K = map(int, input().split())
+
+coin = []
+for _ in range(N):
+    coin.append(int(input()))
+
+def cash(num):
+    count = []
+    idx = len(coin)-1
+    target = coin[idx]
+
+    while (num != 0):
+        if (target > num): # 가장 큰 값의 동전이 현재 가격보다 크다면
+            idx -= 1
+            target = coin[idx]
+
+        else:
+            count.append(num // target)
+            num -= count[-1] * target
+
+    return sum(count)
+
+print(cash(K))
