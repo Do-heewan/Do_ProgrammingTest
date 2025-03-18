1dd9a908325f054c54bf43174cd0e72e5802532b
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-18 22:08:11 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,21 @@
+# 15666 N과 M (12)
+
+from itertools import combinations
+
+N, M = map(int, input().split())
+seq = list(set(map(int, input().split()))) # set 중복 제거 수열 입력
+seq.sort()
+
+s = []
+def dfs(x):
+    if (len(s) == M):
+        print(*s)
+        return
+    
+    for i in range(x, len(seq)):
+        s.append(seq[i])
+        # print(f"현재 상태 : {s}")
+        dfs(i)
+        s.pop()
+
+dfs(0)
