39080e51181e84c066ac5d9a04521ae3e8f7419f
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-03-25 21:33:03 +0900

    [Gold V] Title: 용액, Time: 120 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,30 +1,31 @@
-# 2470 두 용액 (두 포인터)
+# 2467 용액
 
 N = int(input())
-li = list(map(int, input().split()))
-li.sort()
+liquid = list(map(int, input().split()))
+liquid.sort()
 
 start, end = 0, N-1
 
-answer = abs(li[start] + li[end]) # 가장 작은 합
-result = [li[start], li[end]] # 결과 리스트
+answer = abs(liquid[start] + liquid[end]) # 두 용액의 합
+result = [liquid[start], liquid[end]] # 두 용액 저장 리스트
 
 while (start < end):
-    s_val = li[start] # 제일 작은 값
-    e_val = li[end] # 제일 큰 값
+    min = liquid[start] # 가장 작은 용액
+    max = liquid[end] # 가장 큰 용액
 
-    sum = s_val + e_val
+    sum = min + max # 두 용액의 합
 
-    if (abs(sum) < answer): # 처음의 합과 이후 연산을 통한 합 비교
-        answer = abs(sum) # 더 작은 값이 answer에 들어감
-        result = [s_val, e_val] # 그때의 두 용액
+    if (abs(sum) < answer):
+        answer = abs(sum)
+        result = [min, max]
 
-        if (answer == 0): # 0이면 최솟값이기에 break
+        if (answer == 0): # 합이 0이면 무조건 최솟값
             break
-    
-    if (sum < 0): # 음수면 작은 값을 증가시킴
+
+    if (sum < 0): # 합이 음수일 경우, 그 다음 작은 용액으로 변경
         start += 1
-    else: # 양수면 큰 값을 감소시킴
+
+    else: # 합이 양수일 경우, 그 다음 큰 용액으로 변경
         end -= 1
 
-print(result[0], result[1]) # 결과 리스트 출력
+print(result[0], result[1])
