15e0615a47a5a64cc330ca2eb0560714ade6436d
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-04-04 19:18:49 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2464 ms, Memory: 210716 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,47 @@
+# 1197 최소 스패닝 트리
+
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+
+V, E = map(int, input().split())
+
+tree = []
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    tree.append((a, b, c))
+tree.sort(key = lambda x : x[2]) # c(cost)에 대해서 오름차순 정렬
+
+parent = [i for i in range(V+1)]
+
+def get_parent(x):
+    if (parent[x] == x):
+        return x
+
+    parent[x] = get_parent(parent[x])
+
+    return parent[x]
+
+def union_parent(a, b):
+    a = get_parent(a)
+    b = get_parent(b)
+
+    if (a < b):
+        parent[b] = a
+
+    else:
+        parent[a] = b
+
+def same_parent(a, b):
+    return get_parent(a) == get_parent(b)
+
+answer = 0
+cost_target = 0
+for a, b, cost in tree:
+    if not (same_parent(a, b)):
+        union_parent(a, b)
+        answer += cost
+        if (cost > cost_target):
+            cost_target = cost
+
+print(answer - cost_target)
