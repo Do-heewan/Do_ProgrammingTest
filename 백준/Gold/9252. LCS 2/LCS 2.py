7f679a8da167390323a7664c89affab25539e57c
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-04-26 20:18:30 +0900

    [Gold IV] Title: LCS 2, Time: 708 ms, Memory: 420772 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,19 @@
+# 9252 LCS 2
+
+first = [''] + list(input())
+second = [''] + list(input())
+
+dp = [['' for _ in range(len(second))] for _ in range(len(first))]
+
+for i in range(1, len(first)):
+    for j in range(1, len(second)):
+        if first[i] == second[j]:
+            dp[i][j] = dp[i-1][j-1] + first[i]
+        else:
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+
+result = dp[-1][-1]
+print(len(result), result, sep="\n")
