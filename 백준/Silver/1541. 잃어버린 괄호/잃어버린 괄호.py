254d5a6e000a0fe090b0e352af7d95ea69713193
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-05-08 21:08:45 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,17 +1,19 @@
 # 1541 잃어버린 괄호
 
-seq = input().split("-")
+seq = input().split("-") # -를 기준으로 split
 
 num = []
 
 for ix in seq:
     sum = 0
-    tmp = ix.split("+")
-    for i in tmp:
-        sum += int(i)
+    tmp = ix.split("+") # -로 나눈 애들 중, +로 다시 나눔
+
+    for i in tmp: # 나눠진 애들은 모두 숫자
+        sum += int(i) # 합해준다.
     
-    num.append(sum)
-n = num[0]
+    num.append(sum) # 더한 값을 리스트에 저장
+
+n = num[0] # num 리스트에는 모두 정수. 따라서 첫 번째 값에서 다른 값들을 빼준다.
 
 for i in range(1, len(num)):
     n -= num[i]
