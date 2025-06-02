9c61b44d118589ee897cb82ad1cf6487d0b1706e
noh <nhw3152@gmail.com>
2025-06-02 16:58:58 +0900

    25-1 프로그래밍 경진대회 답안

--- 25-1 프로그래밍경진대회/스마트폰_내구성_테스트.1748591738959/Main.py
+++ 25-1 프로그래밍경진대회/스마트폰_내구성_테스트.1748591738959/Main.py
@@ -1,15 +1,13 @@
 def min_trials(N, K):
-    # K를 10cm 단위로 나눈 단계 수
     steps = K // 10
-    
-    # dp[t][n] = t번 시도, n개의 장비로 확인 가능한 최대 단계 수
-    dp = [[0] * (N + 1) for _ in range(steps + 2)]  # +2는 범위 초과 방지
 
-    for t in range(1, steps + 2):  # 시도 횟수는 최대 steps까지
-        for n in range(1, N + 1):
-            # 점화식: dp[t][n] = dp[t-1][n-1] + dp[t-1][n] + 1
-            dp[t][n] = dp[t - 1][n - 1] + dp[t - 1][n] + 1
-            if dp[t][n] >= steps:
+    dp = [[0] * (N+1) for _ in range(steps+1)]
+
+    for t in range(1, steps+1):
+        for n in range(1, N+1):
+            dp[t][n] = dp[t-1][n-1] + dp[t-1][n] + 1
+            
+            if (dp[t][n] >= steps):
                 return t  # 최소 시도 횟수 반환
     return steps  # 최악의 경우 모든 단계 다 확인
 
