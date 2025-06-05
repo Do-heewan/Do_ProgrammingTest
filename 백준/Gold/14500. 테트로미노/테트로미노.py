b8550832c0e787df5d1b6c4fe4662e4324d9874f
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-06-06 00:37:06 +0900

    [Gold IV] Title: 테트로미노, Time: 784 ms, Memory: 116624 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,56 +1,62 @@
-#백준 14500
-#테트로미노
+# 14500 테트로미노
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)] # 입력값 저장
-visited = [[False] * m for _ in range(n)] # 방문확인
+N, M = map(int, input().split())
 
-# 방향설정
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
-maximum = 0 # 최댓값 저장 변수
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
 
-# ㅗ 모양을 제외한 나머지 모양 탐색
+visited = [[False] * M for _ in range(N)]
+
+maximum = 0
+
 def dfs(x, y, tmp, cnt):
     global maximum
-    if cnt == 4: # 탐색완료 후 최댓값 비교
+
+    if (cnt == 4):
         maximum = max(maximum, tmp)
         return
-    for i in range(4): # 방향 탐색
+    
+    for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx<0 or nx>=m or ny<0 or ny>=n or visited[ny][nx]:
-            continue
-        visited[ny][nx] = True # 방문처리
-        dfs(nx, ny, tmp+graph[ny][nx], cnt+1)
-        visited[ny][nx] = False # 방문처리 제거
 
-# ㅗ 모양 탐색
-def fy(x, y):
+        if (0 <= nx < N) and (0 <= ny < M) and not visited[nx][ny]:
+            visited[nx][ny] = True
+            dfs(nx, ny, tmp + graph[nx][ny], cnt + 1)
+            visited[nx][ny] = False
+
+def fuck_you(x, y):
     global maximum
-    tmp = graph[y][x]
     arr = []
-    for i in range(4): # 모든 방향 탐색
+
+    for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx<0 or nx>=m or ny<0 or ny>=n:
-            continue
-        arr.append(graph[ny][nx])
+
+        if (0 <= nx < N) and (0 <= ny < M) and not visited[nx][ny]:
+            arr.append(graph[nx][ny])
+
     length = len(arr)
-    if length == 4 : # 만약 4방향 모두 nxm에 들어간다면 그중 가장 작은 값 제거 후 sum
+
+    if (length == 4):
         arr.sort(reverse=True)
         arr.pop()
-        maximum = max(maximum, sum(arr) + graph[y][x])
-    elif length == 3: # 3방향만 nxm에 들어가기 때문에 바로 sum
-        maximum = max(maximum, sum(arr) + graph[y][x])
-    return # length가 2 이하라면 ㅗ 모양이 아니므로 바로 return
+        maximum = max(maximum, sum(arr) + graph[x][y])
 
-for i in range(n):
-    for j in range(m):
-        visited[i][j] = True # 현재 지점 방문처리
-        dfs(j, i, graph[i][j], 1)
-        fy(j, i)
+    elif (length == 3):
+        maximum = max(maximum, sum(arr) + graph[x][y])
+    
+    return
+
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = True
+        dfs(i, j, graph[i][j], 1)
+        fuck_you(i, j)
         visited[i][j] = False
 
-print(maximum) # 정답 출력
+print(maximum)
