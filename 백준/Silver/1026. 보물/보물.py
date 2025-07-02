79ba0b34db728cccfb23495efe11b2829fb9b515
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-07-02 16:20:46 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,14 @@
+# 1026 보물
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+sum = 0
+for i in range(N):
+    sum += A[i] * B[i]
+
+print(sum)
