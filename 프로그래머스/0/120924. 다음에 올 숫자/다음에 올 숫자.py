d81fa0f0bed5f112966c073beca2acfabed717b3
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-07-12 16:41:03 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -5,6 +5,6 @@ def solution(common):
         return (common[-1] + common[1] - common[0])
         
     elif (common[1] // common[0] == common[2] // common[1]):
-        return common[len(common) - 1] * common[1] // common[0]
+        return common[-1] * (common[1] // common[0])
     
     return answer
