9673f2c17cfeeecd67050518fedd7082b2fb563f
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-08-23 09:59:30 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,8 +1,7 @@
 def solution(n):
     answer = []
-    for i in range(n, 0, -1):
+    for i in range(1, n+1):
         if (i % 2 == 1):
             answer.append(i)
-    answer.sort()
     
     return answer
