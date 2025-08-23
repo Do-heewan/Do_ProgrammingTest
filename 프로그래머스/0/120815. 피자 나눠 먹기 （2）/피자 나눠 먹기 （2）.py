88859eb99c6cf67b7a65d6de2133f8a74cc95c7d
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-08-23 10:50:20 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,6 +1,8 @@
 def solution(n):
-    cnt = 1
-    while (n != 0):
-        if (cnt * 6) % n == 0:
-            return cnt
-        cnt += 1
+    # cnt = 1
+    # while (n != 0):
+    #     if (cnt * 6) % n == 0:
+    #         return cnt
+    #     cnt += 1
+    
+    
