d777cd104bec7d3b100ac122e854ceac24ef0396
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-08-30 10:11:33 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,7 +1,14 @@
 def solution(slice, n):
-    answer = 0
-    while n > 0:
-        n -= slice
-        answer += 1
+#     answer = 0
+#     while n > 0:
+#         n -= slice
+#         answer += 1
         
-    return answer
+#     return answer
+
+    cnt = 1
+    while True:
+        if (cnt * slice >= n):
+            break
+        cnt += 1
+    return cnt
