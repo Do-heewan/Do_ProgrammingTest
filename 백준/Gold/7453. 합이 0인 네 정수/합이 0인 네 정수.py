0d674b89a728493a1c77cad9fb555f2d7b566c38
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-09-18 12:16:39 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 8752 ms, Memory: 937564 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,18 +1,28 @@
-#sol2
-import sys;input = sys.stdin.readline
+# 7453 합이 0인 네 정수
+
 N = int(input())
-A = [];B = [];C = [];D = []
+
+a, b, c, d = [], [], [], []
 for _ in range(N):
-    a,b,c,d = map(int,input().split())
-    A.append(a);B.append(b);C.append(c);D.append(d)
+    a1, b1, c1, d1 = map(int, input().split())
+    a.append(a1)
+    b.append(b1)
+    c.append(c1)
+    d.append(d1)
+
 dic = {}
-for a in A:
-    for b in B:
-        if a+b in dic:dic[a+b]+=1
-        else:dic[a+b]=1
-ans = 0
-for c in C:
-    for d in D:
-        if -c-d in dic:
-            ans+=dic[-c-d]
-print(ans)
+
+for ax in a:
+    for bx in b:
+        if ax+bx in dic:
+            dic[ax+bx] += 1
+        else:
+            dic[ax+bx] = 1
+
+answer = 0
+for cx in c:
+    for dx in d:
+        if -cx-dx in dic:
+            answer += dic[-cx-dx]
+
+print(answer)
