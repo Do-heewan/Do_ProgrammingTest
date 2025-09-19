83152cb10fc874a96f09374926abbc92f16ff65c
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-09-19 12:42:54 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,20 +1,13 @@
 # 1010 다리 놓기
 
+import math
+
 T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    
-    dp = [[0] * (M+1) for _ in range(N+1)]
 
-    for i in range(1, N+1):
-        for j in range(1, M+1):
-            if i == 1:
-                dp[i][j] = j
-            else:
-                if i == j:
-                    dp[i][j] = 1
-                elif i < j:
-                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+    # nCr = fact(n) / fact(n-r) * fact(r)
+    result = math.factorial(M) / (math.factorial(M-N) * math.factorial(N))
 
-    print(dp[-1][-1])
+    print(int(result))
