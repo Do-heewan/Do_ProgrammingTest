413112f79a499f6156c9cc4bad8c10546dee430d
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-09-27 11:13:41 +0900

    [level 2] Title: 올바른 괄호, Time: 7.85 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -4,11 +4,32 @@ def solution(s):
     for ix in s:
         if ix == "(":
             stack.append(ix)
-        elif ix == ")":
-            if not stack:
-                return False
+        elif ix == ")" and stack:
             stack.pop()
-            
-    if stack:
-        return False
+        else:
+            return False
+    
+    if stack: return False
     return True
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     stack = []
+    
+#     for ix in s:
+#         if ix == "(":
+#             stack.append(ix)
+#         elif ix == ")":
+#             if not stack:
+#                 return False
+#             stack.pop()
+            
+#     if stack:
+#         return False
+#     return True
