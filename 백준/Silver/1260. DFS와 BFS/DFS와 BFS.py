c4a63ce87561f1ab722bea09b792d47d1ff31eaf
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-09-29 13:56:48 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35692 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -5,43 +5,42 @@ import sys
 
 input = sys.stdin.readline
 
-N, M, V = map(int, input().split()) # 정점, 간선, 시작 노드 입력
+def bfs(n):
+    b_visited[n] = True
+    Q = deque()
+    Q.append(n)
+    print(n, end=' ')
 
-graph = [[] for _ in range(N+1)] # 그래프 초기화(생성)
-visited_dfs = [False] * (N+1) # DFS 진행 시 방문하는 노드 표시
-visited_bfs = [False] * (N+1) # BFS 진행 시 방문하는 노드 표시
+    while Q:
+        c = Q.popleft()
 
-# 그래프 생성
+        for ix in graph[c]:
+            if not b_visited[ix]:
+                Q.append(ix)
+                b_visited[ix] = True
+                print(ix, end=' ')
+
+def dfs(n):
+    d_visited[n] = True
+    print(n, end=' ')
+
+    for ix in graph[n]:
+        if not d_visited[ix]:
+            dfs(ix)
+
+N, M, V = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a] += [b]
-    graph[b] += [a]
+    graph[a].append(b)
+    graph[b].append(a)
 
-# 작은 수 부터 탐색을 진행하기 위해 정렬
 for i in range(len(graph)):
     graph[i].sort()
 
-def dfs(v):
-    print(v, end = " ") # 시작 노드 출력
-    visited_dfs[v] = True # 방문 표시
-
-    for ix in graph[v]: # 시작 노드의 다음 노드 진행
-        if not (visited_dfs[ix]): # 방문하지 않은 노드라면
-            dfs(ix) # 재귀 호출
-
-def bfs(v):
-    print(v, end = " ") # 시작 노드 출력
-    visited_bfs[v] = True # 방문 표시
-    Q = deque([v]) # 큐 생성
-
-    while Q: # 큐에 내용이 존재하지 않을 때 까지
-        c = Q.popleft() # 큐의 왼쪽부터 pop
-
-        for ix in graph[c]: # 시작 노드의 왼쪽 자식 노드의 자식 노드들
-            if not (visited_bfs[ix]): # 방문하지 않은 노드라면
-                visited_bfs[ix] = True # 방문 표시
-                print(ix, end = " ") # 출력
-                Q.append(ix) # 큐에 저장
+b_visited = [False] * (N+1)
+d_visited = [False] * (N+1)
 
 dfs(V)
 print()
