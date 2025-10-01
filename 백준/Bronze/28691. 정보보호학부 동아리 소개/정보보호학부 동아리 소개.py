972602ed34d1913f39dd3f3ba51dd24504581108
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-10-01 23:16:11 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+w = input()
+if w == "M":
+    print("MatKor")
+elif w == "C":
+    print("CyKor")
+elif w == "W":
+    print("WiCys")
+elif w =="A":
+    print("AlKor")
+else:
+    print("$clear")
