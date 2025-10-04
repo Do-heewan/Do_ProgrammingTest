e7bd6d6dd6cab410592b021506029ee8c7f5d8de
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-10-04 10:11:32 +0900

    [level 0] Title: 배열 자르기, Time: 0.00 ms, Memory: 9.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.py
@@ -0,0 +1,8 @@
+def solution(numbers, num1, num2):
+#     new_arr = []
+#     for i in range(num1, num2+1):
+#         new_arr.append(numbers[i])
+        
+#     return new_arr
+
+    return numbers[num1:num2+1]
