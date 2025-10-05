768d92f5198edaa517a34c0e2634432be63a4b42
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-10-05 21:18:50 +0900

    [Silver I] Title: 경로 찾기, Time: 340 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,26 +1,26 @@
 # 11403 경로 찾기
 
-N = int(input())
-
 INF = 1_000_000_000
 
-graph = [[INF for _ in range(N+1)] for _ in range(N+1)]
+N = int(input())
+
+graph = [[INF] * (N+1) for _ in range(N+1)]
+
 for i in range(1, N+1):
     lst = list(map(int, input().split()))
-    for j in range(len(lst)):
-        if (lst[j] == 1):
-            graph[i][j+1] = 1
+    for j in range(1, len(lst)+1):
+        if lst[j-1] == 1:
+            graph[i][j] = 1
 
-# 플로이드-워셜 점화식
 for k in range(1, N+1):
-    for a in range(1, N+1):
-        for b in range(1, N+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-for a in range(1, N+1):
-    for b in range(1, N+1):
-        if (graph[a][b] == INF):
-            print(0, end = " ")
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
         else:
-            print(1, end = " ")
+            print(1, end=' ')
     print()
