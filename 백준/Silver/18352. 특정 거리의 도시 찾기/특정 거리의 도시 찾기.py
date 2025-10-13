5ec4f3eb2844d6753780dedfbce5b99ffe86e380
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-10-13 11:33:30 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1320 ms, Memory: 99760 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,37 +1,34 @@
-# 18352 특정 거리의 도시 찾기
-
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def bfs(n):
-    visited[n] = True
-    Q = deque()
-    Q.append(n)
+n , m , k , x = map(int, input().split())
+g = [[] for _ in range(n+1)]
+for i in range(m):
+    a , b = map(int, input().split())
+    g[a].append(b)
+visited = [-1] * (n+1)
+visited[x] = 0
+now = [x]
+next = list()
 
-    while Q:
-        c = Q.popleft()
+for i in range(k):
+    for j in now:
+        for p in g[j]:
+            if visited[p] == -1:
+                visited[p] = i + 1
+                next.append(p)
+    # print(next)
+    now = list(next)
+    next = list()
 
-        for ix in graph[c]:
-            if not visited[ix]:
-                visited[ix] = True
-                distance[ix] = distance[c] + 1
-                Q.append(ix)
+result = []
+for i in now:
+    if visited[i] == k:
+        result.append(i)
 
-N, M, K, X = map(int, input().split())
-
-graph = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-
-visited = [False for _ in range(N+1)]
-distance = [0 for _ in range(N+1)]
-bfs(X)
-
-if K in distance:
-    for i in range(1, N+1):
-        if distance[i] == K:
-            print(i)
+if result:
+    result.sort()  # 오름차순 정렬
+    for city in result:
+        print(city)
 else:
     print(-1)
