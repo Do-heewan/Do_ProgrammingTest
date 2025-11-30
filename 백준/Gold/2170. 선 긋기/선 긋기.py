f68854e4f00ef2d315e95b4f95a4029d3d749961
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-11-30 23:44:35 +0900

    [Gold V] Title: 선 긋기, Time: 2248 ms, Memory: 270540 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -0,0 +1,25 @@
+# 2170 선 긋기
+
+N = int(input())
+line = [list(map(int, input().split())) for _ in range(N)]
+line.sort(key=lambda x : x[0])
+
+tmp = [line[0][0], line[0][1]]
+ans = 0
+for x, y in line:
+    if tmp[0] <= x and y <= tmp[1]:
+        continue
+    elif x < tmp[0] and tmp[1] < y:
+        tmp = [x, y]
+    elif x <= tmp[0] <= y:
+        tmp[0] = x
+    elif x <= tmp[1] <= y:
+        tmp[1] = y
+    else:
+        ans += tmp[1] - tmp[0]
+        tmp = [x, y]
+
+else:
+    if tmp:
+        ans += tmp[1] - tmp[0]
+print(ans)
