57481cf29d0f4b531df7b179804ef6ad9ab79a10
Noh <139729135+Do-heewan@users.noreply.github.com>
2025-12-26 20:02:49 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -7,8 +7,6 @@ time = 0
 for a in alph:
     for dial in dial_list:
         if a in dial:
-            time += dial_list.index(dial)
-
-    time += 1
+            time += dial_list.index(dial) + 1
 
 print(time)
