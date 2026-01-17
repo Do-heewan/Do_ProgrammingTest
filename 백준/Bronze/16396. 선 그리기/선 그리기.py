43f336747232a2c8104878e8f80ed9357a10673c
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-01-17 17:08:03 +0900

    [Bronze I] Title: 선 그리기, Time: 2552 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16396. 선 그리기/선 그리기.py
+++ 백준/Bronze/16396. 선 그리기/선 그리기.py
@@ -1,10 +1,9 @@
-# 16396 선 그리기
-
 N = int(input())
-line = [0] * 10001
+
+lines = [0] * (10001)
 for _ in range(N):
     a, b = map(int, input().split())
     for i in range(a, b):
-        line[i] = 1
+        lines[i] = 1
 
-print(sum(line))
+print(sum(lines))
