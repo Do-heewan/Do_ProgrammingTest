3ad12c97e31470d8562b2562667916030f82fd63
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-02-08 18:47:05 +0900

    [Silver II] Title: 동전 게임, Time: 4856 ms, Memory: 32412 KB, Score: 49 point -BaekjoonHub

---
+++ 백준/Silver/10837. 동전 게임/동전 게임.py
@@ -0,0 +1,21 @@
+# 10837 동전 게임
+
+K = int(input())
+C = int(input())
+
+for _ in range(C):
+    M, N = map(int, input().split())
+
+    if M == N:
+        print(1)
+    elif M < N:
+        if N-M-1 >= K-N+1:
+            print(0)
+        else:
+            print(1)
+
+    elif M > N:
+        if M-N-1 > K-M+1:
+            print(0)
+        else:
+            print(1)
