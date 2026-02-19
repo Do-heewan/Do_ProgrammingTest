d68fba87e5e2dded1de351677529ade7a775e43c
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-02-19 14:36:14 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,19 @@
+# 1032 명령프롬프트
+
+N = int(input())
+
+curr = []
+for _ in range(N):
+    word = input()
+
+    if not curr:
+        curr = list(word)
+        continue
+
+    for i in range(len(curr)):
+        if curr[i] != word[i]:
+            curr[i] = "?"
+        
+
+for c in curr:
+    print(c, end='')
