13d6d8e0406dd89aea7e82cc328b22e2d433c872
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-07 12:53:32 +0900

    [Bronze I] Title: 피보나치 수, Time: 32 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Bronze/4150. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.py
@@ -2,8 +2,10 @@
 
 n = int(input())
 
-dp = [0, 1, 1]
+dp = [0] * (4800+1)
+dp[1] = 1
+dp[2] = 1
 for i in range(3, n+1):
-    dp.append(dp[i-1]+dp[i-2])
+    dp[i] = dp[i-2] + dp[i-1]
 
 print(dp[n])
