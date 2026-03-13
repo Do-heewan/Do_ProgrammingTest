064f74effbd8cfc42f3cb4a7237f1e74055d4051
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-13 21:14:38 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,31 @@
-# 2606 바이러스 (BFS)
+# 2606 바이러스
 
 from collections import deque
 
-n = int(input()) # 노드 갯수(컴퓨터)
-v = int(input()) # 엣지 갯수(연결 선)
+def bfs(n):
+    Q = deque()
+    Q.append(n)
+    visited[n] = True
 
-graph = [[] for i in range(n+1)] # 그래프 초기화
-visited = [0] * (n+1) # 방문한 컴퓨터인지 표시
+    while Q:
+        c = Q.popleft()
 
-for i in range(v):
+        for ix in graph[c]:
+            if not visited[ix]:
+                Q.append(ix)
+                visited[ix] = True
+
+N = int(input())
+M = int(input())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
     a, b = map(int, input().split())
-    graph[a] += [b] # a에 b 연결
-    graph[b] += [a] # b에 a 연결 -> 양방향
+    graph[a].append(b)
+    graph[b].append(a)
 
-visited[1] = 1 # 1번부터 시작이기에 1번 표시
-Q = deque([1])
-while Q:
-    c = Q.popleft()
-    
-    for nx in graph[c]:
-        if (visited[nx] == 0):
-            Q.append(nx)
-            visited[nx] = 1
+visited = [False] * (N+1)
 
-print(sum(visited) - 1)
+bfs(1)
+
+print(sum(visited)-1)
