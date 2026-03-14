9c5973a7a7440d50dcfc1c3d2729811dd8d75f0c
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-14 10:45:51 +0900

    [Gold IV] Title: 최단경로, Time: 636 ms, Memory: 74636 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -5,40 +5,39 @@ input = sys.stdin.readline
 
 import heapq
 
-V, E = map(int, input().split()) # V개의 정점, E개의 간선
+INF = 10_000_000
 
-start = int(input()) # 시작 정점
+def dijkstra(n):
+    heap = []
+    weight[n] = 0
 
-INF = 1_000_000_000 # 아주 큰 값 설정
-graph = [[] for _ in range(V+1)] 
-weight = [INF] * (V+1) # 가중치 합 저장 리스트
+    heapq.heappush(heap, (0, n))
 
-for _ in range(E):
-    a, b, w = map(int, input().split())
-    graph[a].append((b, w)) # 시작 정점에 (끝 정점, 가중치) 저장
+    while heap:
+        wgt, curr = heapq.heappop(heap)
 
-def dijkstra(num):
-    queue = []
-    heapq.heappush(queue, (0, num)) # 시작 위치의 가중치는 0
-    weight[num] = 0
-
-    while queue:
-        wgt, now = heapq.heappop(queue) # 큐에 저장된 가중치와 정점 정보 pop
-
-        if (weight[now] < wgt): # 현재 정점에 저장된 가중치가 더 작다 => 이미 작은 가중치로 현재 정점에 도착함
+        if wgt > weight[curr]:
             continue
 
-        for v, w in graph[now]:
-            cost = wgt + w # 비용 = v로 가는데 드는 가중치 + 현재의 가중치
-            if (cost < weight[v]): # 비용이 더 적게 들면
-                weight[v] = cost # 더 작은 가중치로 변경
-                heapq.heappush(queue, (cost, v)) # 힙에 푸쉬
+        for next, cost in graph[curr]:
+            if weight[next] > wgt + cost:
+                weight[next] = wgt + cost
+                heapq.heappush(heap, (wgt+cost, next))
 
-dijkstra(start)
+V, E = map(int, input().split())
+K = int(input())
 
-for i in range(1, len(weight)):
-    if (weight[i] == INF): # 가중치가 바뀌지 않은 경우, 도달 할 수 없음
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    a, b, cost = map(int, input().split())
+    graph[a].append([b, cost])
+
+weight = [INF] * (V+1)
+
+dijkstra(K)
+
+for w in weight[1:]:
+    if w == INF:
         print("INF")
-
     else:
-        print(weight[i])
+        print(w)
