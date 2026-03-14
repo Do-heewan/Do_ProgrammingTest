7af7f40beddd4ee102ea25badc7be64b27a3a51f
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-14 10:25:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 692 ms, Memory: 67020 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,37 +1,38 @@
-# 11724  연결 요소의 개수 (BFS)
+# 11724 연결 요소의 개수
 
-from collections import deque
 import sys
-
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+from collections import deque
 
-graph = [[] for _ in range(N+1)] # 그래프 초기화
-visited = [False] * (N+1) # 방문 여부
-count = 0 # 트리 탐색
-
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a] += [b] # a에 b 연결
-    graph[b] += [a] # b에 a 연결 -> 양방향
-
-# 넓이 우선 탐색
-def bfs(start_v, visited):
-    Q = deque([start_v])
+def bfs(n):
+    Q = deque()
+    Q.append(n)
+    visited[n] = True
 
     while Q:
         c = Q.popleft()
 
-        for nx in graph[c]:
-            if not (visited[nx]):
-                visited[nx] = True
-                Q.append(nx)
+        for ix in graph[c]:
+            if not visited[ix]:
+                Q.append(ix)
+                visited[ix] = True
 
-    return 1 # 트리 탐색을 마치면 1을 반환
+    return 1
 
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (N+1)
+
+res = 0
 for i in range(1, N+1):
-    if not (visited[i]):
-        count += bfs(i, visited)
+    if not visited[i]:
+        res += bfs(i)
 
-print(count)
+print(res)
