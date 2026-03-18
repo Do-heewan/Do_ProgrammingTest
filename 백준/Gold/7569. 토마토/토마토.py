30a353ac3db2847a240c38383c2f0a7bf5dfcf91
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-18 11:40:35 +0900

    [Gold V] Title: 토마토, Time: 2192 ms, Memory: 53536 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,57 +1,59 @@
 # 7569 토마토
 
 import sys
+input = sys.stdin.readline
+
 from collections import deque
-# input = sys.stdin.readline
+
+def bfs(tomatoes):
+    Q = deque()
+    
+    for x, y, h in tomatoes:
+        Q.append([x, y, h])
+
+    while Q:
+        cx, cy, ch = Q.popleft()
+
+        for i in range(6):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            nh = ch + dh[i]
+
+            if 0 <= nx < N and 0 <= ny < M and 0 <= nh < H and graph[nh][ny][nx] == 0:
+                if graph[nh][ny][nx] == 0:
+                    graph[nh][ny][nx] = graph[ch][cy][cx] + 1
+                    Q.append([nx, ny, nh])
 
 N, M, H = map(int, input().split())
 
-# 상, 하, 좌, 우, 앞, 뒤
-dx = [0, 0, -1, 1, 0, 0]
-dy = [1, -1, 0, 0, 0, 0]
-dh = [0, 0, 0, 0, 1, -1]
+dx = [0, 0, 0, 0, -1, 1]
+dy = [0, 0, 1, -1, 0, 0]
+dh = [1, -1, 0, 0, 0, 0]
 
 graph = []
 for _ in range(H):
-    graph_2 = []
+    g = []
     for _ in range(M):
-        graph_2.append(list(map(int, input().split())))
-    graph.append(graph_2)
+        g.append(list(map(int, input().split())))
+    graph.append(g)
 
-visited = [[[False for _ in range(N)] for _ in range(M)] for _ in range(H)]
-Q = deque()
-
-# 토마토 위치 찾기
+tomatoes = []
 for h in range(H):
     for y in range(M):
         for x in range(N):
-            if (graph[h][y][x] == 1):
-                Q.append([h, y, x])
-                visited[h][y][x] = 1
+            if graph[h][y][x] == 1:
+                tomatoes.append([x, y, h])
 
-while Q:
-    ch, cy, cx = Q.popleft()
+bfs(tomatoes)
 
-    for i in range(6):
-        nh = ch + dh[i]
-        ny = cy + dy[i]
-        nx = cx + dx[i]
-
-        if (0 <= nh < H) and (0 <= ny < M) and (0 <= nx < N) and (graph[nh][ny][nx] == 0) and not visited[nh][ny][nx]:
-            Q.append([nh, ny, nx])
-            visited[nh][ny][nx] = visited[ch][cy][cx] + 1
-
+ans = 0
 for h in range(H):
     for y in range(M):
         for x in range(N):
-            if not visited[h][y][x] and graph[h][y][x] == 0:
+            if graph[h][y][x] == 0:
                 print(-1)
-                exit(0)
+                exit()
+            else:
+                ans = max(ans, graph[h][y][x])
 
-max_value = 0
-for h in range(H):
-    for y in range(M):
-        for x in range(N):
-            max_value = max(max_value, visited[h][y][x])
-
-print(max_value - 1 if max_value != 1 else 0)
+print(ans-1)
