a6a5521b0556601c31a8667a7038c318908c05a6
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-21 10:01:17 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3788 ms, Memory: 192180 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,40 +1,45 @@
 # 2206 벽 부수고 이동하기
 
-import sys
 from collections import deque
-input = sys.stdin.readline
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 
-def bfs(i, j, w):
+def bfs():
     Q = deque()
-    Q.append([i, j, w])
-    visited[i][j][w] = 1
+    Q.append([0, 0, 0])
+    visited[0][0][0] = 1
 
     while Q:
-        cx, cy, cw = Q.popleft()
+        cx, cy, isBreak = Q.popleft()
 
-        if cx == N-1 and cy == M-1:
-            return visited[cx][cy][cw]
-        
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph[nx][ny] == 0 and visited[nx][ny][cw] == 0:
-                    visited[nx][ny][cw] = visited[cx][cy][cw] + 1
-                    Q.append([nx, ny, cw])
-                
-                elif graph[nx][ny] == 1 and cw == 0 and visited[nx][ny][cw] == 0:
-                    visited[nx][ny][1] = visited[cx][cy][cw] + 1
-                    Q.append([nx, ny, 1])
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny][isBreak] == -1:
+                # 벽이 아니라면
+                if graph[nx][ny] == "0":
+                    Q.append([nx, ny, isBreak])
+                    visited[nx][ny][isBreak] = visited[cx][cy][isBreak]+1
 
-    return -1
+                # 벽이라면 부술지 안부술지 정하기
+                elif graph[nx][ny] == "1" and isBreak == 0:
+                    Q.append([nx, ny, 1])
+                    visited[nx][ny][1] = visited[cx][cy][0]+1
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(N)]
-visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+graph = [list(input()) for _ in range(N)]
 
-print(bfs(0, 0, 0))
+visited = [[[-1, -1] for _ in range(M)] for _ in range(N)]
+
+bfs()
+
+if visited[N-1][M-1][0] == -1 and visited[N-1][M-1][1] == -1:
+    print(-1)
+elif visited[N-1][M-1][0] == -1:
+    print(visited[N-1][M-1][1])
+elif visited[N-1][M-1][1] == -1:
+    print(visited[N-1][M-1][0])
+else:
+    print(min(visited[N-1][M-1][0], visited[N-1][M-1][1]))
