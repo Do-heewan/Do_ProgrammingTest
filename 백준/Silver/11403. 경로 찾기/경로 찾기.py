86b0826488ca6a5ed10dd2fab4bf9ae356ffa61e
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-21 12:54:34 +0900

    [Silver I] Title: 경로 찾기, Time: 240 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,31 +1,13 @@
-# 11403 경로 찾기
-
-import sys
-input = sys.stdin.readline
-
-from collections import deque
+# 11403 경로 찾기 (플로이드 워셜)
 
 N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-graph = [[] for _ in range(N)]
-for i in range(N):
-    lst = list(map(int, input().split()))
-    for j in range(N):
-        if lst[j] == 1:
-            graph[i].append(j)
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] == 1 and graph[k][j] == 1:
+                graph[i][j] = 1
 
-for i in range(N):
-    visited = [0] * N
-
-    Q = deque()
-    Q.append(i)
-    
-    while Q:
-        c = Q.popleft()
-
-        for ix in graph[c]:
-            if not visited[ix]:
-                visited[ix] = 1
-                Q.append(ix)
-
-    print(*visited)
+for g in graph:
+    print(*g)
