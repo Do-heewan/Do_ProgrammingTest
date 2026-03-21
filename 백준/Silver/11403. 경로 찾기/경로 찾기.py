301e297a9106e6a7ba833783728d1016e67bd23f
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-21 12:49:42 +0900

    [Silver I] Title: 경로 찾기, Time: 76 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,26 +1,31 @@
 # 11403 경로 찾기
 
-INF = 1_000_000_000
+import sys
+input = sys.stdin.readline
+
+from collections import deque
 
 N = int(input())
 
-graph = [[INF] * (N+1) for _ in range(N+1)]
-
-for i in range(1, N+1):
+graph = [[] for _ in range(N)]
+for i in range(N):
     lst = list(map(int, input().split()))
-    for j in range(1, len(lst)+1):
-        if lst[j-1] == 1:
-            graph[i][j] = 1
+    for j in range(N):
+        if lst[j] == 1:
+            graph[i].append(j)
 
-for k in range(1, N+1):
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+for i in range(N):
+    visited = [0] * N
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if graph[i][j] == INF:
-            print(0, end=' ')
-        else:
-            print(1, end=' ')
-    print()
+    Q = deque()
+    Q.append(i)
+    
+    while Q:
+        c = Q.popleft()
+
+        for ix in graph[c]:
+            if not visited[ix]:
+                visited[ix] = 1
+                Q.append(ix)
+
+    print(*visited)
