3e88acd9323af03169e3fff66f4b4edb16cda2b8
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-22 18:06:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 268 ms, Memory: 57288 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,44 +1,35 @@
-# 1197 최소 스패닝 트리
+# 1197 최소 스패닝 트리 (Kruskal)
 
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 9)
+
+sys.setrecursionlimit(10 ** 8)
+
+def find_root(x):
+    if x != parent[x]:
+        parent[x] = find_root(parent[x])
+    return parent[x]
+
+def union(a, b):
+    x = find_root(a)
+    y = find_root(b)
+    if x > y: parent[x] = y
+    else: parent[y] = x
+
+def same_root(x, y):
+    return find_root(x) == find_root(y)
 
 V, E = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(E)]
 
-tree = []
-for _ in range(E):
-    a, b, c = map(int, input().split())
-    tree.append((a, b, c))
-tree.sort(key = lambda x : x[2]) # c(cost)에 대해서 오름차순 정렬
+graph.sort(key=lambda x : x[2])
 
 parent = [i for i in range(V+1)]
 
-def get_parent(x):
-    if (parent[x] == x):
-        return x
+cost = 0
+for a, b, c in graph:
+    if not same_root(a, b):
+        union(a, b)
+        cost += c
 
-    parent[x] = get_parent(parent[x])
-
-    return parent[x]
-
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
-
-    if (a < b):
-        parent[b] = a
-
-    else:
-        parent[a] = b
-
-def same_parent(a, b):
-    return get_parent(a) == get_parent(b)
-
-answer = 0
-for a, b, cost in tree:
-    if not (same_parent(a, b)):
-        union_parent(a, b)
-        answer += cost
-
-print(answer)
+print(cost)
