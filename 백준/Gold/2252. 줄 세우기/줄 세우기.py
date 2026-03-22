b091d5d75e5442620ea860c94cefa751755715b7
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-22 17:23:17 +0900

    [Gold III] Title: 줄 세우기, Time: 2560 ms, Memory: 41656 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,40 +1,31 @@
 # 2252 줄 세우기
 
-import sys
-from collections import deque
-input = sys.stdin.readline
+import heapq
 
 N, M = map(int, input().split())
-# 모든 노드에 대한 진입차수는 0으로 초기화
-indegree = [0] * (N + 1)
-# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트
-graph = [[] for _ in range(N + 1)]
 
+graph = [[] for _ in range(N+1)]
+indegree = [0] * (N+1)
 for _ in range(M):
-    A, B = map(int, input().split())
-    graph[A].append(B)
-    indegree[B] += 1
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
 
-# 위상 정렬 함수
-def topology_sort():
-    result = []
-    Q = deque()
+heap = []
 
-    for i in range(1, N+1):
-        if (indegree[i] == 0):
-            Q.append(i)
+for i in range(1, N+1):
+    if indegree[i] == 0:
+        heapq.heappush(heap, i)
 
-    while Q:
-        now = Q.popleft()
-        result.append(now)
-        # 해당 원소와 연결된 노드들의 진입차수에서 1빼기
-        for g in graph[now]:
-            indegree[g] -= 1
-            if (indegree[g] == 0):
-                Q.append(g)
+result = []
+while heap:
+    curr = heapq.heappop(heap)
+    result.append(curr)
 
-    # 위상 정렬 수행한 결과 출력
-    for res in result:
-        print(res, end=' ')
+    for next_ in graph[curr]:
+        indegree[next_] -= 1
 
-topology_sort()
+        if indegree[next_] == 0:
+            heapq.heappush(heap, next_)
+
+print(*result)
