b9c3901c9040ae974435d6e2f1391d11b85cad83
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-23 12:15:09 +0900

    [Silver I] Title: 나이트의 이동, Time: 2872 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,36 +1,37 @@
-# 7562 나이트의 이동
-
 import sys
 input = sys.stdin.readline
-
 from collections import deque
 
-move = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
+move = [(1, 2), (1, -2), (-1, 2), (-1, -2),
+        (2, 1), (2, -1), (-2, 1), (-2, -1)]
 
 T = int(input())
 
 for _ in range(T):
     L = int(input())
-    curr = list(map(int, input().split()))
-    target = list(map(int, input().split()))
+    curr = tuple(map(int, input().split()))
+    target = tuple(map(int, input().split()))
+
+    if curr == target:
+        print(0)
+        continue
 
     graph = [[-1] * L for _ in range(L)]
 
-    Q = deque()
-    Q.append((curr[0], curr[1]))
+    Q = deque([curr])
     graph[curr[0]][curr[1]] = 0
 
     while Q:
         cx, cy = Q.popleft()
 
-        if [cx, cy] == target:
+        if (cx, cy) == target:
             print(graph[cx][cy])
             break
 
-        for m in move:
-            nx = cx + m[0]
-            ny = cy + m[1]
+        for dx, dy in move:
+            nx = cx + dx
+            ny = cy + dy
 
             if 0 <= nx < L and 0 <= ny < L and graph[nx][ny] == -1:
+                graph[nx][ny] = graph[cx][cy] + 1
                 Q.append((nx, ny))
-                graph[nx][ny] = graph[cx][cy]+1
