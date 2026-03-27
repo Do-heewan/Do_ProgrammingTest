d9ce55f65bf582bd683999d5aeaa52fc5574af92
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-27 11:23:16 +0900

    [Silver I] Title: 신입 사원, Time: 5620 ms, Memory: 53032 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,4 +1,4 @@
-# 1946 신입 사원
+# 1946 신입사원
 
 import sys
 input = sys.stdin.readline
@@ -10,18 +10,15 @@ for _ in range(T):
 
     people = []
     for _ in range(N):
-        a, b = map(int, input().split())
-        people.append((a, b))
-    
-    people.sort(key = lambda x : x[0]) # 서류 성적이 낮은 순 정렬
+        people.append(list(map(int, input().split())))
 
-    pass_num = 1 # 통과한 사람 수
-    target = people[0][1] # 서류 성적이 젤 낮은 사람의 면접 등수
-    
-    # 등수 비교
-    for i in range(1, len(people)):
-        if (target > people[i][1]): # 등수가 더 크다 -> 순위가 낮다 -> 서류 성적도 낮고 면접 순위도 낮다
-            pass_num += 1 # 합격자 한명 추가
-            target = people[i][1] # 타겟 변경
+    people.sort()
 
-    print(pass_num)
+    cnt = 0
+    curr_b = N+1
+    for a, b in people:
+        if b < curr_b:
+            curr_b = b
+            cnt += 1
+
+    print(cnt)
