34ac93183ffd93f47a7f9cf67f345539c40b2c52
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-27 21:01:16 +0900

    [Silver IV] Title: 큐 2, Time: 1644 ms, Memory: 144256 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,35 @@
+# 18258 큐2
+
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+N = int(input())
+
+queue = deque()
+for _ in range(N):
+    cmd = list(input().split())
+
+    if cmd[0] == "push":
+        num = cmd[1]
+        queue.append(num)
+
+    elif cmd[0] == "size":
+        print(len(queue))
+
+    elif cmd[0] == "empty":
+        print(0 if queue else 1)
+
+    elif cmd[0] == "pop":
+        if queue:
+            a = queue.popleft()
+            print(a)
+        else:
+            print(-1)
+
+    elif cmd[0] == "front":
+        print(queue[0] if queue else -1)
+
+    elif cmd[0] == "back":
+        print(queue[-1] if queue else -1)
