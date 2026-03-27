8d4616db48ead2c889e1220d26a2f3fe0e0d389c
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-27 18:15:34 +0900

    [Gold V] Title: 선 긋기, Time: 2864 ms, Memory: 251688 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -7,24 +7,21 @@ N = int(input())
 line = [list(map(int, input().split())) for _ in range(N)]
 line.sort(key=lambda x : x[0])
 
-tmp = [line[0][0], line[0][1]]
+start, end = line[0]
 ans = 0
+
 for i in range(1, N):
     x, y = line[i]
-    
-    if tmp[0] <= x and y <= tmp[1]:
-        continue
-    elif x < tmp[0] and tmp[1] < y:
-        tmp = [x, y]
-    elif x <= tmp[0] <= y:
-        tmp[0] = x
-    elif x <= tmp[1] <= y:
-        tmp[1] = y
-    else:
-        ans += tmp[1] - tmp[0]
-        tmp = [x, y]
 
-else:
-    if tmp:
-        ans += tmp[1] - tmp[0]
-print(ans)
+    # 그여진 선과 현재 선이 겹치면
+    if x <= end:
+        end = max(end, y)
+
+    # 안 겹치면
+    else:
+        ans += end-start
+        start, end = line[i]
+
+ans += end-start
+
+print(ans)    
