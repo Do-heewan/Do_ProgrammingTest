c5b675c8ca249517d0e89364f504c41a8ec3696f
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-27 20:58:06 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,39 +1,33 @@
 # 10828 스택
 
 import sys
-
 input = sys.stdin.readline
+
 N = int(input())
 
-li = []
-
+stack = []
 for _ in range(N):
-    cmd = input().strip().split()
+    cmd = list(input().split())
 
-    if (len(cmd) == 2):
-        op, x = cmd[0], cmd[1]
+    if cmd[0] == "push":
+        num = cmd[1]
+        stack.append(num)
 
-        if (op == "push"):
-            li.append(x)
-
-    if (cmd[0] == "pop"):
-        if (len(li) == 0):
+    elif cmd[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
             print(-1)
-        else:
-            print(li[-1])
-            del li[-1]
 
-    elif (cmd[0] == "top"):
-        if (len(li) == 0):
+    elif cmd[0] == "size":
+        print(len(stack))
+
+    elif cmd[0] == "empty":
+        print(0 if stack else 1)
+
+    elif cmd[0] == "pop":
+        if stack:
+            a = stack.pop()
+            print(a)
+        else:
             print(-1)
-        else:
-            print(li[-1])
-
-    elif (cmd[0] == "size"):
-        print(len(li))
-
-    elif (cmd[0] == "empty"):
-        if (len(li) == 0):
-            print(1)
-        else:
-            print(0)
