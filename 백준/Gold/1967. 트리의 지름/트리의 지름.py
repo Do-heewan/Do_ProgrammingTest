9315bfff8610f902b18d7cda519a7c3ec5611916
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-29 18:01:39 +0900

    [Gold IV] Title: 트리의 지름, Time: 84 ms, Memory: 38580 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -2,36 +2,53 @@
 
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
+
+INF = 1_000_000_000
+
+import heapq
+
+def dijkstra(n):
+    weight[n] = 0
+
+    heap = []
+    heapq.heappush(heap, [0, n])
+
+    while heap:
+        wgt, curr = heapq.heappop(heap)
+
+        if wgt > weight[curr]:
+            continue
+
+        for next_, c in graph[curr]:
+            cost = c + wgt
+            if cost < weight[next_]:
+                weight[next_] = cost
+                heapq.heappush(heap, [cost, next_])
 
 N = int(input())
 
-graph = [[] for _ in range(N+1)] # 트리 초기화
-
+graph = [[] for _ in range(N+1)]
 for _ in range(N-1):
-    a, b, c = map(int, input().split())
-    graph[a].append([b, c])
-    graph[b].append([a, c])
+    a, b, cost = map(int, input().split())
+    graph[a].append([b, cost])
+    graph[b].append([a, cost])
 
-# 방문(길이) 표시
-visited = [-1 for _ in range(N+1)]
-visited[1] = 0 # 루트 노드에선 길이 0
+weight = [INF] * (N+1)
+dijkstra(1)
 
-# DFS 탐색
-def dfs(num, length):
-    for a, b in graph[num]:
-        if (visited[a] == -1):
-            visited[a] = b + length # 현재의 길이와 하위 노드로 갈 때의 길이 합을 저장
-            dfs(a, visited[a])
+max_distance = 0
+far_node = 0
+for idx, wgt in enumerate(weight[1:]):
+    if wgt > max_distance:
+        far_node = idx+1
+        max_distance = wgt
 
-# 루트에서부터 모든 노드까지의 거리 탐색
-dfs(1, 0)
+weight = [INF] * (N+1)
+dijkstra(far_node)
 
-start = visited.index(max(visited)) # 루트에서 부터 가장 멀리 떨어진 노드
+max_distance = 0
+for wgt in weight:
+    if wgt == INF: continue
+    max_distance = max(max_distance, wgt)
 
-visited = [-1 for _ in range(N+1)] # 방문 초기화
-visited[start] = 0
-
-dfs(start, 0) # start로부터 모든 노드까지의 거리 탐색
-
-print(max(visited)) # 가장 긴 길이가 트리의 지름이 된다.
+print(max_distance)
