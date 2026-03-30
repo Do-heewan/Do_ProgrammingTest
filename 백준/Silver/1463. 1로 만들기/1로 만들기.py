bb777deeaae3d171261ab50ac8bb6a265c73363b
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-30 22:18:19 +0900

    [Silver III] Title: 1로 만들기, Time: 476 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,14 @@
-# 1463 1로 만들기
-
 N = int(input())
 
-dp = [10 ** 6] * (N+1)
-dp[N] = 0
+dp = [0] * (N+1)
+dp[1] = 0
 
-for i in range(N, 0, -1):
-    if i % 3 == 0:
-        dp[i // 3] = min(dp[i // 3], dp[i] + 1)
+for i in range(2, N+1):
+    dp[i] = dp[i-1]+1
+
     if i % 2 == 0:
-        dp[i // 2] = min(dp[i // 2], dp[i] + 1)
-    dp[i-1] = min(dp[i-1], dp[i]+1)
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
 
-print(dp[1])
+print(dp[N])
