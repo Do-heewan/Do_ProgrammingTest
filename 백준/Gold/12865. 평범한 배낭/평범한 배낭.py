ebaa20fe58f66770eb61c6d0c4670bf9e732821f
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-31 09:36:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 2504 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,13 @@
 # 12865 평범한 배낭
 
 N, K = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(N)]
 
-items = []
-for _ in range(N):
-    w, v = map(int, input().split())
-    items.append([w, v])
+dp = [0] * (K+1)
+dp[0] = 0
 
-knapsack = [0] * (K+1)
 for w, v in items:
     for i in range(K, w-1, -1):
-        knapsack[i] = max(knapsack[i], knapsack[i-w] + v)
+        dp[i] = max(dp[i], dp[i-w]+v)
 
-print(max(knapsack))
+print(max(dp))
