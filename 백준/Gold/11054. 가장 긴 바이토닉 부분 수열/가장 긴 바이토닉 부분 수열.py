43251a43ef39e3836b4c82e0abc8f7563042fc15
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-31 11:54:22 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,24 +1,18 @@
-# 11054 가징 긴 바이토닉 부분 수열
+# 11054 가장 긴 바이토닉 부분 수열
 
 N = int(input())
 lst = list(map(int, input().split()))
-rev_lst = lst[::-1]
 
-dpMax = [1] * N
-dpMin = [1] * N
+dp = [1] * N
 
-for i in range(N):
+for i in range(1, N):
     for j in range(i):
         if lst[i] > lst[j]:
-            dpMax[i] = max(dpMax[i], dpMax[j]+1)
-        
-        if rev_lst[i] > rev_lst[j]:
-            dpMin[i] = max(dpMin[i], dpMin[j]+1)
+            dp[i] = max(dp[i], dp[j]+1)
 
-dpMin = dpMin[::-1]
+for i in range(1, N):
+    for j in range(i):
+        if lst[i] < lst[j]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-result = []
-for i in range(N):
-    result.append(dpMax[i] + dpMin[i])
-
-print(max(result) - 1)
+print(max(dp))
