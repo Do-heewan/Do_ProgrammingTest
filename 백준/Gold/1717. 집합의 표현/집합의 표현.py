50857adcab0f1687933416748930f7eb2d4c573d
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-31 15:55:26 +0900

    [Gold V] Title: 집합의 표현, Time: 272 ms, Memory: 77876 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,32 +1,36 @@
 # 1717 집합의 표현
 
 import sys
-sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10 ** 8)
 
 def find_root(x):
     if (parent[x] == x): return x
     parent[x] = find_root(parent[x])
     return parent[x]
 
-def same_root(a, b):
-    return find_root(a) == find_root(b)
-
-def union_set(a, b):
+def union(a, b):
     x = find_root(a)
     y = find_root(b)
-    if (x > y):
-        parent[x] = y 
-    else: 
+
+    if x > y:
+        parent[x] = y
+    else:
         parent[y] = x
+
+def same_root(x, y):
+    return find_root(x) == find_root(y)
 
 N, M = map(int, input().split())
 
 parent = [i for i in range(N+1)]
+
 for _ in range(M):
     cmd, a, b = map(int, input().split())
 
-    if (cmd == 0):
-        union_set(a, b)
-
-    elif (cmd == 1):
-        print("YES") if same_root(a, b) else print("NO")
+    if cmd == 0:
+        if not same_root(a, b):
+            union(a, b)
+    else:
+        print("YES" if same_root(a, b) else "NO")
