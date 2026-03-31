4a3e79021a1b8ba62340b9687188fc1cfce75b2f
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-31 11:29:35 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 216 ms, Memory: 110780 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
@@ -1,21 +1,29 @@
 # 2624 동전 바꿔주기
 
-import sys
-input = sys.stdin.readline
-
 T = int(input())
 k = int(input())
 coins = [list(map(int, input().split())) for _ in range(k)]
 
-dp = [[0] * (k+1) for _ in range(T+1)]
-dp[0][0] = 1
+dp = [0] * (T+1)
+dp[0] = 1
 
-for i in range(1, k+1):
-    curr_coin, cnt = coins[i-1]
+for c, cnt in coins:
+    for money in range(T, 0, -1):
+        for i in range(1, cnt+1):
+            if money - c * i >= 0:
+                dp[money] += dp[money-c*i]
 
-    for money in range(T+1):
-        for j in range(cnt+1):
-            if money - curr_coin*j >= 0:
-                dp[money][i] += dp[money-curr_coin*j][i-1]
+print(dp[T])
 
-print(dp[T][k])
+# dp = [[0] * (k+1) for _ in range(T+1)]
+# dp[0][0] = 1
+
+# for i in range(1, k+1):
+#     curr_coin, cnt = coins[i-1]
+
+#     for money in range(T+1):
+#         for j in range(cnt+1):
+#             if money - curr_coin*j >= 0:
+#                 dp[money][i] += dp[money-curr_coin*j][i-1]
+
+# print(dp[T][k])
