5302a810e88fe1ada7571d37a5f05ca9d4ea0e49
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-31 10:39:06 +0900

    [Gold III] Title: 앱, Time: 464 ms, Memory: 49880 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -6,15 +6,32 @@ cost = list(map(int, input().split()))
 
 MAX_COST = sum(cost)
 
-dp = [0] * (MAX_COST+1)
+# dp = [0] * (MAX_COST+1)
 
-for i in range(N):
-    b = byte[i]
-    c = cost[i]
-    for curr_cost in range(MAX_COST, c-1, -1):
-        dp[curr_cost] = max(dp[curr_cost], dp[curr_cost-c]+b)
+# for i in range(N):
+#     b = byte[i]
+#     c = cost[i]
+#     for curr_cost in range(MAX_COST, c-1, -1):
+#         dp[curr_cost] = max(dp[curr_cost], dp[curr_cost-c]+b)
 
-for i in range(MAX_COST+1):
-    if dp[i] >= M:
-        print(i)
+# for i in range(MAX_COST+1):
+#     if dp[i] >= M:
+#         print(i)
+#         break
+
+dp = [[0] * (MAX_COST+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    curr_byte = byte[i-1]
+    curr_cost = cost[i-1]
+
+    for j in range(MAX_COST+1):
+        if j < curr_cost:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-curr_cost]+curr_byte)
+
+for j in range(MAX_COST+1):
+    if dp[N][j] >= M:
+        print(j)
         break
