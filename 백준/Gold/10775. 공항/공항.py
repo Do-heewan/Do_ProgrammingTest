4412d57f272266b4f8b1614acdf7597d252017b5
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-03-31 17:44:05 +0900

    [Gold II] Title: 공항, Time: 140 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -3,24 +3,25 @@
 import sys
 input = sys.stdin.readline
 
+sys.setrecursionlimit(10 ** 8)
+
 def find_root(x):
-    if x == parent[x]: return parent[x]
+    if x == parent[x]: return x
     parent[x] = find_root(parent[x])
     return parent[x]
 
 G = int(input())
 P = int(input())
 
+plane = [int(input()) for _ in range(P)]
+
 parent = [i for i in range(G+1)]
 
 ans = 0
-for _ in range(P):
-    g = int(input())
+for p in plane:
+    root = find_root(p)
 
-    root = find_root(g)
-
-    if root == 0:
-        break
+    if root == 0: break
 
     parent[root] = find_root(root-1)
     ans += 1
