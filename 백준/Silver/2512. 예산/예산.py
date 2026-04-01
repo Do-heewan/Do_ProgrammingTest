dc769a927c2fadc3edcb110382f157ee10954394
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-01 22:04:07 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,43 @@
+# 2512 예산
+
+def check(mid):
+    total_money = 0
+    for m in money:
+        if m < mid:
+            total_money += m
+        else:
+            total_money += mid
+
+    if total_money <= limit:
+        return True
+    else:
+        return False
+
+        
+
+def binary_search(start, end):
+    answer = 0
+
+    while start <= end:
+        mid = (start+end) // 2
+
+        if check(mid):
+            answer = mid
+            start = mid+1
+        else:
+            end = mid-1
+
+    return answer
+
+N = int(input())
+money = list(map(int, input().split()))
+limit = int(input())
+
+if sum(money) <= limit:
+    print(max(money))
+
+else:
+    start = 0
+    end = max(money)
+
+    print(binary_search(start, end))
