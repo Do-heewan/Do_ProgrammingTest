656101f26f973012d16df9681b259bb4eaa2e734
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-01 17:56:19 +0900

    [Gold V] Title: 용액, Time: 84 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,31 +1,25 @@
 # 2467 용액
 
+def binary_search(arr, start, end):
+    min_value = float('inf')
+    result = []
+
+    while start < end:
+        curr = arr[start] + arr[end]
+
+        if abs(curr) < min_value:
+            min_value = abs(curr)
+            result = [arr[start], arr[end]]
+
+        if curr < 0:
+            start += 1
+        else:
+            end -= 1
+
+    return result
+
 N = int(input())
-liquid = list(map(int, input().split()))
-liquid.sort()
+lst = list(map(int, input().split()))
+lst.sort()
 
-start, end = 0, N-1
-
-answer = abs(liquid[start] + liquid[end]) # 두 용액의 합
-result = [liquid[start], liquid[end]] # 두 용액 저장 리스트
-
-while (start < end):
-    min = liquid[start] # 가장 작은 용액
-    max = liquid[end] # 가장 큰 용액
-
-    sum = min + max # 두 용액의 합
-
-    if (abs(sum) < answer):
-        answer = abs(sum)
-        result = [min, max]
-
-        if (answer == 0): # 합이 0이면 무조건 최솟값
-            break
-
-    if (sum < 0): # 합이 음수일 경우, 그 다음 작은 용액으로 변경
-        start += 1
-
-    else: # 합이 양수일 경우, 그 다음 큰 용액으로 변경
-        end -= 1
-
-print(result[0], result[1])
+print(*binary_search(lst, 0, N-1))
