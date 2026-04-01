969a8f77b4410196ef35e357a031ac173e2d0f19
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-01 21:39:26 +0900

    [Silver IV] Title: 듣보잡, Time: 2420 ms, Memory: 44628 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,24 +1,11 @@
 # 1764 듣보잡
 
-import sys
+N, M = map(int, input().split())
+hear = [input() for _ in range(N)]
+look = [input() for _ in range(M)]
 
-n, m = map(int, (sys.stdin.readline().split()))
-# n, m = map(int, (input().split()))
+hear_look = set(hear) & set(look)
 
-n_list = set()
-m_list = set()
-
-for i in range(n):
-    name1 = sys.stdin.readline().rstrip()
-    n_list.add(name1)
-for j in range(m):
-    name2 = sys.stdin.readline().rstrip()
-    m_list.add(name2)
-
-sum_list = n_list & m_list
-print(len(sum_list))
-
-sum_list = list(sum_list)
-sum_list.sort()
-for k in sum_list:
-    print(k)
+print(len(hear_look))
+for ix in sorted(list(hear_look)):
+    print(ix)
