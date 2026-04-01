ce78091f1c1f10b8c2e4594083ab4ac99c18f8dd
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-01 11:23:24 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,22 +1,42 @@
-import sys
-input = sys.stdin.readline
+# 1043 거짓말
 
-n, m = map(int, input().split())
-knowList = set(input().split()[1:])
-parties = []
+def find_root(x):
+    if x == parent[x]: return x
+    parent[x] = find_root(parent[x])
+    return parent[x]
 
-for _ in range(m):
-    parties.append(set(input().split()[1:]))
+def union(a, b):
+    x = find_root(a)
+    y = find_root(b)
 
-for _ in range(m):
-    for party in parties:
-        if party & knowList:
-            knowList = knowList.union(party)
+    if x > y:
+        parent[x] = y
+    else:
+        parent[y] = x
 
+def same_root(x, y):
+    return find_root(x) == find_root(y)
+
+N, M = map(int, input().split())
+real = list(map(int, input().split()))
+party = [list(map(int, input().split())) for _ in range(M)]
+
+parent = [i for i in range(N+1)]
+
+# 1. 파티 사람들간의 관계 형성
+for p in party:
+    for i in range(2, len(p)):
+        union(p[1], p[i])
+
+# 2. 진실을 아는 사람 추출
+truth_people = set(find_root(x) for x in real[1:])
+
+# 3. 각 파티 검사
 cnt = 0
-for party in parties:
-    if party & knowList:
-        continue
-    cnt += 1
+for p in party:
+    if not p[1:]: continue
+
+    if find_root(p[1]) not in truth_people:
+        cnt += 1
 
 print(cnt)
