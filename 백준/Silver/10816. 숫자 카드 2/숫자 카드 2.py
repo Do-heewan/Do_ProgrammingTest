ba329bf037cada6fd370369630ff224dd627dcf0
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-01 17:23:31 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 804 ms, Memory: 117016 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,14 +1,17 @@
 # 10816 숫자 카드 2
 
 from collections import Counter
-import sys
-input = sys.stdin.readline
 
 N = int(input())
-N_list = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 
 M = int(input())
-M_list = list(map(int, input().split()))
+target = list(map(int, input().split()))
 
-count = Counter(N_list)
-print(" ".join(str(count[i]) if i in count else "0" for i in M_list))
+num_set = Counter(nums)
+
+for t in target:
+    if t in num_set.keys():
+        print(num_set[t])
+    else:
+        print(0)
