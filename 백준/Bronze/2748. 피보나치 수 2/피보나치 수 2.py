d590373a59d2dbe2d72a8c6dbc8b9341fa5ffd3e
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-02 16:00:48 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -3,10 +3,10 @@
 N = int(input())
 
 dp = [0] * 91
-dp[0] = 0
 dp[1] = 1
+dp[2] = 1
 
-for i in range(2, N+1):
-  dp[i] = dp[i-1] + dp[i-2]
+for i in range(3, N+1):
+    dp[i] = dp[i-1]+dp[i-2]
 
 print(dp[N])
