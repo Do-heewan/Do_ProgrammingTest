9958d468322f2d35c884cf4a33931c91e61189ef
Noh <139729135+Do-heewan@users.noreply.github.com>
2026-04-03 13:37:46 +0900

    [Gold III] Title: 세 용액, Time: 176 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -4,34 +4,26 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-liquid = list(map(int, input().split()))
-liquid.sort()
+lst = list(map(int, input().split()))
+lst.sort()
 
-answer = sys.maxsize
+answer = 10 ** 15
+result = [0, 0, 0]
 
 for i in range(N-2):
-    start = i+1
-    end = N-1
+    left = i+1
+    right = N-1
 
-    while (start < end):
-        fix = liquid[i] # 용액 고정
-        min = liquid[start] # 고정 용액을 제외한 가장 작은 용액
-        max = liquid[end] # 고정 용액을 제외한 가장 큰 용액
+    while left < right:
+        curr = lst[i] + lst[left] + lst[right]
 
-        sum = fix + min + max # 용액 합
+        if abs(curr) < abs(answer):
+            answer = curr
+            result = [lst[i], lst[left], lst[right]]
 
-        if (abs(sum) < answer):
-            answer = abs(sum)
-            result = [liquid[i], min, max]
+        if curr > 0:
+            right -= 1
+        else:
+            left += 1
 
-        if (sum < 0): # 합이 음수일 경우, 그 다음 작은 용액으로 변경
-            start += 1
-
-        elif (sum > 0): # 합이 양수일 경우, 그 다음 큰 용액으로 변경
-            end -= 1
-        
-        else: # 합이 0일 경우, 가장 작은 경우
-            break
-
-for ix in result:
-    print(ix, end = ' ')
+print(*result)
